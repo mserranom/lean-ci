5526111e4c73028a4b56445d0b166d2acf41d0d6
mserranom <mserranom@users.noreply.github.com>
2015-06-08 16:31:28 +0100

    fixed malformed url for repos

--- src/main/ts/builder.ts
+++ src/main/ts/builder.ts
@@ -11,23 +11,22 @@ export module builder {
 
     var terminalAPI = new terminal.TerminalAPI(config.terminal, config.sshPubKey);
 
-    var commands = [
-        "git clone https://github.com/",
-        "cd lean-ci",
-        "git clean -xfd",
-        "npm install --unsafe-perm"];
-
-
     export function startBuild(repo : String) {
 
+        let commands = [
+            "git clone https://github.com/",
+            "cd lean-ci",
+            "git clean -xfd",
+            "npm install --unsafe-perm"];
+            
         console.log('starting build on repo: ' + repo);
-        commands[0] = commands[0] + repo + '.git';
+        commands[0] += repo + '.git';
 
         terminalAPI.createTerminal()
             .then(terminal =>
             {
                 console.log('key: ' + terminal.container_key);
-                var agentURL = terminal.subdomain + ".terminal.com";
+                let agentURL = terminal.subdomain + ".terminal.com";
                 ssh.execute(agentURL, commands)
                     .then(() => terminalAPI.closeTerminal(terminal))
                     .fail(error => console.log("error creating terminal:  " + error.message));
