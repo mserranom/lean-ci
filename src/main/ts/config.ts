5d81add9326f0da21102346da5d1f765304e9274
Miguel Serrano <miguel.serrano@plumbee.co.uk>
2015-06-24 16:29:34 +0100

    extracted config reading

--- src/main/ts/config.ts
+++ src/main/ts/config.ts
@@ -1,17 +1,37 @@
-export var config = {
+///<reference path="../../../lib/node-0.10.d.ts"/>
 
-    appUrl : '',
-    defaultPort : 64321,
 
-    terminal: {
-        userToken: '',
-        accessToken: '',
-        buildAgentId: ''
-    },
+import fs = require("fs");
 
-    github : {
-        username : '',
-        password : '',
-        hookUrl : '',
-    }
-};
+export var config;
+
+var path = process.env.HOME + '/lean-ci-config.json';
+console.log('looking up config.json in ' + path);
+
+if(fs.existsSync(path)) {
+    console.log(path + ' found');
+    config = JSON.parse(fs.readFileSync(path, 'utf8'));
+} else {
+
+    console.log(path + ' not found, applying default config');
+
+    config = {
+
+        appUrl : '',
+        defaultPort : 64321,
+
+        terminal: {
+            userToken: '',
+            accessToken: '',
+            buildAgentId: ''
+        },
+
+        github : {
+            username : '',
+            password : '',
+            hookUrl : '',
+        }
+    };
+}
+
+
