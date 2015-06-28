df4ad96d73eb5fdbefaa5f076714c32bd5304cd9
Miguel Serrano <miguel.serrano@plumbee.co.uk>
2015-06-28 11:01:16 +0100

    added config lookup in local folder

--- src/main/ts/config.ts
+++ src/main/ts/config.ts
@@ -6,11 +6,16 @@ import fs = require("fs");
 export var config;
 
 var path = process.env.HOME + '/lean-ci-config.json';
+var path2 = './lean-ci-config.json';
 console.log('looking up config.json in ' + path);
+console.log('looking up config.json in ' + path2);
 
 if(fs.existsSync(path)) {
     console.log(path + ' found');
     config = JSON.parse(fs.readFileSync(path, 'utf8'));
+} if(fs.existsSync(path2)) {
+    console.log(path2 + ' found');
+    config = JSON.parse(fs.readFileSync(path2, 'utf8'));
 } else {
 
     console.log(path + ' not found, applying default config');
