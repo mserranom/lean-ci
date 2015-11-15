db99f35e5a862be2b243a73cc94813d774df05d2
Miguel Serrano <miguel.serrano@plumbee.co.uk>
2015-11-15 12:18:32 +0000

    added fake method to mark a build as finished

--- src/test/ts/support/SchedulerProcessFake.ts
+++ src/test/ts/support/SchedulerProcessFake.ts
@@ -21,6 +21,16 @@ export class SchedulerProcessFake {
         await this.repo.updateQ({_id : build._id}, build)
     }
 
+    async markAsFinished(buildId : string) {
+        let build : model.Build = await this.repo.fetchFirstQ({_id : buildId});
+        build.commit = 'commit_id';
+        build.config = {dependencies : []};
+        build.processedTimestamp = new Date();
+        build.finishedTimestamp = new Date();
+        build.log = 'build Log SUCCESS';
+        await this.repo.updateQ({_id : build._id}, build);
+    }
+
     async finishOldestRunningBuildWithSuccess() {
         let builds : Array<model.Build> = await this.queue.runningBuilds(this.userId,1 ,1);
         let build = builds[0];
