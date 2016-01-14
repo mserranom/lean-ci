5aff5d1e3f8157f07c978fb510357f6e83bf729c
Miguel Serrano <miguel.serrano@plumbee.co.uk>
2016-01-14 15:29:54 +0000

    added test creating 2 pipelines

--- src/test/ts/integration/single_build_requested_test.ts
+++ src/test/ts/integration/single_build_requested_test.ts
@@ -59,6 +59,47 @@ describe('addition of a new repository', () => {
         done();
     });
 
+    it('POST a second build request should create new pipelines and jobs',  async function(done) {
+
+        const JOB_GENERATED_ID = '2';
+
+        await createRepository();
+
+        let pipeline : model.PipelineSchema = await doPost('/build_requests', {repo : testRepo, commit : 'commit-123'});
+
+        expect(pipeline.jobs).deep.equal([JOB_GENERATED_ID]);
+        expect(pipeline.dependencies).deep.equal([]);
+        expect(pipeline.status).equals(model.PipelineStatus.RUNNING);
+
+        let build : model.Build = await doGet('/builds/' + JOB_GENERATED_ID);
+        expect(build._id).equals(parseInt(JOB_GENERATED_ID));
+        expect(build.repo).equals(testRepo);
+        expect(build.status).equals(model.BuildStatus.QUEUED);
+
+        let requestedPipeline:model.PipelineSchema = await doGet('/pipelines/' + pipeline._id);
+        expect(requestedPipeline).deep.equal(pipeline);
+
+
+        const SECOND_JOB_GENERATED_ID = '3';
+
+        let pipeline2 : model.PipelineSchema = await doPost('/build_requests', {repo : testRepo, commit : 'commit-124'});
+
+        expect(pipeline2.jobs).deep.equal(['3']);
+        expect(pipeline2.dependencies).deep.equal([]);
+        expect(pipeline2.status).equals(model.PipelineStatus.RUNNING);
+
+        build = await doGet('/builds/' + SECOND_JOB_GENERATED_ID);
+        expect(build._id).equals(parseInt(SECOND_JOB_GENERATED_ID));
+        expect(build.repo).equals(testRepo);
+        expect(build.status).equals(model.BuildStatus.QUEUED);
+
+        requestedPipeline = await doGet('/pipelines/' + pipeline2._id);
+        expect(requestedPipeline).deep.equal(pipeline2);
+
+
+        done();
+    });
+
 });
 
 
